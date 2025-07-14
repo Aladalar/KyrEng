968b5425f554daa492ab2ab9dd39241747b8671f
Aladalar <Aladalar@gmail.com>
2025-07-14 14:21:05 +0200

    Mep meta edit -> Need to rework system

--- src/main/java/engine/core/Main.java
+++ src/main/java/engine/core/Main.java
@@ -2,8 +2,6 @@ package engine.core;
 import static com.raylib.Colors.*;
 import static com.raylib.Raylib.*;
 
-import game.scene.SimpleScene;
-
 public class Main {
 
   public static boolean isDebug = true;
@@ -12,16 +10,9 @@ public class Main {
     InitWindow(800,450,"Jaylib VSCode Demo");
     SetTargetFPS(60);
 
-     // Step 1: Create and load scene
-        SimpleScene scene = new SimpleScene();
-        scene.load();  // even if it's empty for now
-        System.out.println("Working dir: " + System.getProperty("user.dir"));
-
-   
     while (!WindowShouldClose()) {
       BeginDrawing();
         ClearBackground(DARKGRAY);
-        scene.render();
 
         DrawText("FPS: " + GetFPS(), 10, 10, 20, DARKGREEN);
       EndDrawing();
